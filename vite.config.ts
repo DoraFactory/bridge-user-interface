1ea5ac13e46a5ee423faa2fd7d65a425164cf387
VegeBun-csj <csj1071285741@gmail.com>
2024-09-20 23:10:14 +0900

    Remove env config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  define: {
-    'process.env': {},
-  },
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
